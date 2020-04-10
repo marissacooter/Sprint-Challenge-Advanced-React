15437f1207827d0592625961dc5e6dfd9419c4a9
Marissa <marissacooterl@gmail.com>
2020-04-10 11:28:10 -0500

    got first test up and working

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {
+  render,
+  fireEvent,
+  getByText,
+  getByTestId,
+} from "@testing-library/react";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test("Testing to confirm that player cards are visible", () => {
+  const { getByTestId } = render(<App />);
+  getByTestId("Player");
+});
+
+
